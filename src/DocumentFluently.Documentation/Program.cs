a7f3ea13af123ba58cd78ccd192d7b23aae01802
nloum <39776302+nloum@users.noreply.github.com>
2020-05-25 10:42:29 -0500

    Put together the basics of a static site generator

--- src/DocumentFluently.Documentation/Program.cs
+++ src/DocumentFluently.Documentation/Program.cs
@@ -1,7 +1,10 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using IoFluently;
-using Markdig;
+using LiveLinq;
 using ReactiveProcesses;
+using System.Reactive.Linq;
+using SimpleMonads;
 
 namespace DocumentFluently.Documentation
 {
@@ -9,25 +12,67 @@ namespace DocumentFluently.Documentation
     {
         static void Main(string[] args)
         {
-            var ioService = new IoService(new ReactiveProcessFactory());
+            var service = new IoService(new ReactiveProcessFactory());
+            var repositoryRoot = service.CurrentDirectory.Ancestors().First(ancestor => (ancestor / ".git").Exists());
 
-            var repoRoot = ioService.CurrentDirectory.Ancestors()
-                .First(ancestor => (ancestor / "src" / "DocumentFluently.sln").Exists());
-            
-            var readmeMarkdownFile = (repoRoot / "readme.md").AsMarkdownFile();
-            var readmeHtmlFile = readmeMarkdownFile.Path.WithExtension(".html").AsHtmlFile();
+            var foldersWithAReadMeFile = repositoryRoot.ToLiveLinq()
+                .Where((path, type) => path.Name == "readme.md" && type == PathType.File)
+                .KeysAsSet()
+                .Select(path => path.Parent());
 
-            var markdown = readmeMarkdownFile.Read();
-            var html = markdown.ToHtml(x =>
-                x.UseAdvancedExtensions()
-                    .UseDiagrams()
-                    .Build());
-            html = new Html(new[]{"<html><body><script src=\"https://cdn.jsdelivr.net/npm/mermaid@8.4.0/dist/mermaid.min.js\"></script><script>mermaid.initialize({startOnLoad:true,securityLevel:'loose'});</script>"}
-                .Concat(html.Lines)
-                .Concat(new[]{"</body></html>"}));
-            readmeHtmlFile.Write(html);
+            var children = foldersWithAReadMeFile
+                .GroupBy(path => path.Parent());
+                // .Subscribe((parent, children) =>
+                //     children.Subscribe(child => Console.WriteLine($"{parent} + {child}"), (child, _) => Console.WriteLine($"{parent} - {child}")),
+                //     (parent, children, disposable) => disposable.Dispose());
+
+            var markdownFiles = repositoryRoot.ToLiveLinq()
+                .Where((path, type) => path.HasExtension(".md") && type == PathType.File)
+                .KeysAsSet()
+                .Select(path => path.AsSmallTextFile());
             
-            var readmePdfFile = readmeHtmlFile.wkhtmltopdf();
+            var html = markdownFiles.Select(markdownFile => {
+                    var markdown = markdownFile.Read();
+                    var Html = Markdig.Markdown.ToHtml(markdown);
+                    return new { markdownFile.Path, Html };
+                });
+
+            html
+                .Select(x => {
+                    return children[x.Path.Parent()]
+                        .SelectLatest(children => {
+                            return children.Select(y => y.ToObservableStateAndChange().Select(childrenStateAndChange => new { Children = childrenStateAndChange.State, x.Path, x.Html } ))
+                                .OtherwiseEmpty();
+                        });
+                })
+                .Subscribe(addedHtml => {
+                    Console.WriteLine($"Adding a markdown file: {addedHtml.Path} with {addedHtml.Children.Count} children");
+                    // foreach(var child in addedHtml.Children) {
+                    //     Console.WriteLine($"- {child}");
+                    // }
+                    //addedHtml.Path.WithExtension(".html").WriteAllText(addedHtml.Html);
+                }, (removedHtml, removalMode) => {
+                    Console.WriteLine($"Removing a markdown file: {removedHtml.Path}");
+                    //removedHtml.Path.WithExtension(".html").Delete();
+                });
+            
+            // var ioService = new IoService(new ReactiveProcessFactory());
+            //
+            // var repoRoot = ioService.CurrentDirectory.Ancestors()
+            //     .First(ancestor => (ancestor / "src" / "DocumentFluently.sln").Exists());
+            //
+            // var readmeMarkdownFile = (repoRoot / "readme.md").AsMarkdownFile();
+            // var readmeHtmlFile = readmeMarkdownFile.Path.WithExtension(".html").AsHtmlFile();
+            //
+            // var markdown = readmeMarkdownFile.Read();
+            // var html = markdown.ToHtml(x =>
+            //     x.UseAdvancedExtensions()
+            //         .UseDiagrams()
+            //         .Build());
+            // html = new Html(new[]{"<html><body><script src=\"https://cdn.jsdelivr.net/npm/mermaid@8.4.0/dist/mermaid.min.js\"></script><script>mermaid.initialize({startOnLoad:true,securityLevel:'loose'});</script>"}
+            //     .Concat(html.Lines)
+            //     .Concat(new[]{"</body></html>"}));
+            // readmeHtmlFile.Write(html);
         }
     }
 }
