514b9a3b5527feeba3cf3bc67365a5152615e44f
nloum <39776302+nloum@users.noreply.github.com>
2020-06-09 08:16:10 -0500

    Get DocumentFluently command line generating html with backlinks

--- src/DocumentFluently.Documentation/Program.cs
+++ src/DocumentFluently.Documentation/Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using IoFluently;
 using LiveLinq;
 using ReactiveProcesses;
 using System.Reactive.Linq;
+using System.Xml;
 using SimpleMonads;
 
 namespace DocumentFluently.Documentation
@@ -14,65 +17,105 @@ namespace DocumentFluently.Documentation
         {
             var service = new IoService(new ReactiveProcessFactory());
             var repositoryRoot = service.CurrentDirectory.Ancestors().First(ancestor => (ancestor / ".git").Exists());
+            var documentationRoot = repositoryRoot / "docs";
 
-            var foldersWithAReadMeFile = repositoryRoot.ToLiveLinq()
-                .Where((path, type) => path.Name == "readme.md" && type == PathType.File)
-                .KeysAsSet()
-                .Select(path => path.Parent());
+            var markdownFiles = documentationRoot.Descendants()
+                .ToLiveLinq()
+                .Where(x => x.HasExtension(".md") && x.GetPathType() == PathType.File)
+                .Select(x => x.AsSmallTextFile());
 
-            var children = foldersWithAReadMeFile
-                .GroupBy(path => path.Parent());
-                // .Subscribe((parent, children) =>
-                //     children.Subscribe(child => Console.WriteLine($"{parent} + {child}"), (child, _) => Console.WriteLine($"{parent} - {child}")),
-                //     (parent, children, disposable) => disposable.Dispose());
-
-            var markdownFiles = repositoryRoot.ToLiveLinq()
-                .Where((path, type) => path.HasExtension(".md") && type == PathType.File)
-                .KeysAsSet()
-                .Select(path => path.AsSmallTextFile());
-            
-            var html = markdownFiles.Select(markdownFile => {
+            var htmls = markdownFiles
+                .Select(markdownFile =>
+                {
                     var markdown = markdownFile.Read();
-                    var Html = Markdig.Markdown.ToHtml(markdown);
-                    return new { markdownFile.Path, Html };
+                    var html = "<html>" + Markdig.Markdown.ToHtml(markdown) + "</html>";
+                    var document = new XmlDocument();
+                    document.LoadXml(html);
+                    return new {markdownFile.Path, Html = document};
                 });
 
-            html
-                .Select(x => {
-                    return children[x.Path.Parent()]
-                        .SelectLatest(children => {
-                            return children.Select(y => y.ToObservableStateAndChange().Select(childrenStateAndChange => new { Children = childrenStateAndChange.State, x.Path, x.Html } ))
-                                .OtherwiseEmpty();
-                        });
+            var backLinks = htmls.SelectMany(html =>
+                {
+                    var links = html.Html.SelectNodes("//a");
+                    var backLinksForThisFile = new List<Tuple<AbsolutePath, AbsolutePath, string>>();
+                    foreach (var link in links)
+                    {
+                        var linkEl = link as XmlElement;
+                        var href = linkEl.Attributes["href"].InnerText;
+                        var text = linkEl.InnerText;
+                        if (href.Contains(":"))
+                        {
+                            continue;
+                        }
+
+                        var maybePath = service.TryParseRelativePath(href);
+                        if (!maybePath.HasValue)
+                        {
+                            continue;
+                        }
+
+                        var linkTo = (html.Path.Parent() / maybePath.Value).Simplify();
+                        backLinksForThisFile.Add(Tuple.Create(html.Path, linkTo, text));
+                    }
+
+                    return backLinksForThisFile.AsEnumerable();
                 })
+                .GroupBy(x => x.Item2)
+                .SelectValue(x => x.Select(y => Tuple.Create(y.Item1, y.Item3)))
+                .ToReadOnlyObservableDictionary();
+            
+            var transformedHtmls = htmls.Select(html => backLinks.ToLiveLinq()[html.Path]
+                .SelectLatest(x =>
+                {
+                    return x.Select(y => y.ToObservableState()).Otherwise(() =>
+                            Observable.Return(ImmutableHashSet<Tuple<AbsolutePath, string>>.Empty));
+                })
+                .Select(backLinksForThisHtml =>
+            {
+                var links = html.Html.SelectNodes("//a");
+                foreach (var link in links)
+                {
+                    var linkEl = link as XmlElement;
+                    var href = linkEl.Attributes["href"].InnerText;
+                    var text = linkEl.InnerText;
+                    if (href.Contains(":"))
+                    {
+                        continue;
+                    }
+
+                    var maybePath = service.TryParseRelativePath(href);
+                    if (!maybePath.HasValue)
+                    {
+                        continue;
+                    }
+
+                    if (linkEl.Attributes["href"].InnerText.EndsWith(".md"))
+                    {
+                        linkEl.Attributes["href"].InnerText =
+                            linkEl.Attributes["href"].InnerText
+                                .Substring(0, linkEl.Attributes["href"].InnerText.Length - 3) + ".html";
+                    }
+                }
+                
+                var backLinkHtml =
+                    "<ul>" + string.Join("\n",
+                        backLinksForThisHtml.Select(x =>
+                        {
+                            var relativePath = x.Item1.WithExtension(".html").RelativeTo(documentationRoot);
+                            return
+                                    $"<li>This page is \"{x.Item2}\" of - <a href=\"{relativePath}\">{relativePath}</a></li>";
+                        })) + "</ul>";
+                return new {html.Path, Html = html.Html.OuterXml + backLinkHtml};
+            }));
+            
+            transformedHtmls
                 .Subscribe(addedHtml => {
-                    Console.WriteLine($"Adding a markdown file: {addedHtml.Path} with {addedHtml.Children.Count} children");
-                    // foreach(var child in addedHtml.Children) {
-                    //     Console.WriteLine($"- {child}");
-                    // }
-                    //addedHtml.Path.WithExtension(".html").WriteAllText(addedHtml.Html);
+                    Console.WriteLine($"Adding a markdown file: {addedHtml.Path}");
+                    addedHtml.Path.WithExtension(".html").WriteAllText(addedHtml.Html);
                 }, (removedHtml, removalMode) => {
                     Console.WriteLine($"Removing a markdown file: {removedHtml.Path}");
-                    //removedHtml.Path.WithExtension(".html").Delete();
+                    removedHtml.Path.WithExtension(".html").Delete();
                 });
-            
-            // var ioService = new IoService(new ReactiveProcessFactory());
-            //
-            // var repoRoot = ioService.CurrentDirectory.Ancestors()
-            //     .First(ancestor => (ancestor / "src" / "DocumentFluently.sln").Exists());
-            //
-            // var readmeMarkdownFile = (repoRoot / "readme.md").AsMarkdownFile();
-            // var readmeHtmlFile = readmeMarkdownFile.Path.WithExtension(".html").AsHtmlFile();
-            //
-            // var markdown = readmeMarkdownFile.Read();
-            // var html = markdown.ToHtml(x =>
-            //     x.UseAdvancedExtensions()
-            //         .UseDiagrams()
-            //         .Build());
-            // html = new Html(new[]{"<html><body><script src=\"https://cdn.jsdelivr.net/npm/mermaid@8.4.0/dist/mermaid.min.js\"></script><script>mermaid.initialize({startOnLoad:true,securityLevel:'loose'});</script>"}
-            //     .Concat(html.Lines)
-            //     .Concat(new[]{"</body></html>"}));
-            // readmeHtmlFile.Write(html);
         }
     }
 }
